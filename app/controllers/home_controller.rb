f681dd0b707ea7aa4144c9f987589e470f675675
Andrew James <andrewjames@Andrews-MacBook-Pro.local>
2015-01-22 15:02:35 -0700

    Scaffold aliens object

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
 
+  def index
+    peoplellist = Person.new
+
+    orglist = Orginization.new
+
+    @people_orginizations = [peoplelist, orglist]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :aliens
+
   resources :orginizations
 
   resources :people
